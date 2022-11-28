9d5d8f4fabd2796a137307b5d9e1477834d18b73
Dankoy <transingularity@gmail.com>
2022-11-28 15:28:26 +0300

    refactor: добавил отдельный класс для чтения из файла csv

--- hw-1/src/main/java/ru/dankoy/core/dao/csv/CsvQuestionsDaoImpl.java
+++ hw-1/src/main/java/ru/dankoy/core/dao/csv/CsvQuestionsDaoImpl.java
@@ -1,80 +1,63 @@
 package ru.dankoy.core.dao.csv;
 
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReaderBuilder;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import ru.dankoy.core.csvreader.CsvReader;
 import ru.dankoy.core.dao.QuestionsDao;
-import ru.dankoy.core.dao.QuestionsDaoException;
 import ru.dankoy.core.domain.Answer;
 import ru.dankoy.core.domain.Question;
 
 /**
  * @author Dankoy
  * <p>
- * Implementation of {@link QuestionsDao} interface where resource for questions is csv file.
+ * Implementation of {@link QuestionsDao} interface which uses {@link CsvReader} as raw reading csv
+ * class
  */
 public class CsvQuestionsDaoImpl implements QuestionsDao {
 
-  private final String resource; // строка с указанием на ресурс
+  private final CsvReader csvReader;
 
-  public CsvQuestionsDaoImpl(String resource) {
-    this.resource = resource;
+  public CsvQuestionsDaoImpl(CsvReader csvReader) {
+    this.csvReader = csvReader;
   }
 
   @Override
   public List<Question> getQuestions() {
 
-    try (var reader = new BufferedReader(
-        new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream(resource))));
-        var csvReader = new CSVReaderBuilder(reader)
-            .withSkipLines(1)
-            .withCSVParser(new CSVParserBuilder()
-                .withSeparator(';')
-                .withIgnoreQuotations(true)
-                .build())
-            .build()) {
+    List<String[]> rows = csvReader.read();
 
-      List<String[]> rows = csvReader.readAll();
+    List<Question> questions = new ArrayList<>();
+    // get amount of questions
+    for (String[] row : rows) {
 
-      List<Question> questions = new ArrayList<>();
-      // get amount of questions
-      for (String[] row : rows) {
+      var questionId = Long.parseLong(row[0]);
+      var questionText = row[1];
 
-        var questionId = Long.parseLong(row[0]);
-        var questionText = row[1];
+      // parse answers. may have different amount
+      var correctAnswerId = -1;
+      List<Answer> answers = new ArrayList<>();
+      var answerId = 1;
+      for (int i = 2; i < row.length; i += 2) {
 
-        // parse answers. may have different amount
-        var correctAnswerId = -1;
-        List<Answer> answers = new ArrayList<>();
-        var answerId = 1;
-        for (int i = 2; i < row.length; i += 2) {
+        if (!row[i].isEmpty() || !row[i].isBlank()) {
+          answers.add(new Answer(answerId, row[i]));
 
-          if (!row[i].isEmpty() || !row[i].isBlank()) {
-            answers.add(new Answer(answerId, row[i]));
+          var isCorrect = Boolean.parseBoolean(row[i + 1]);
 
-            var isCorrect = Boolean.parseBoolean(row[i + 1]);
-
-            if (isCorrect) {
-              correctAnswerId = answerId;
-            }
+          if (isCorrect) {
+            correctAnswerId = answerId;
           }
-          answerId++;
-
         }
-
-        var question = new Question(questionId, questionText, answers, correctAnswerId);
-        questions.add(question);
+        answerId++;
 
       }
 
-      return questions;
+      var question = new Question(questionId, questionText, answers, correctAnswerId);
+      questions.add(question);
 
-    } catch (Exception e) {
-      throw new QuestionsDaoException(e);
     }
+
+    return questions;
+
   }
 }
