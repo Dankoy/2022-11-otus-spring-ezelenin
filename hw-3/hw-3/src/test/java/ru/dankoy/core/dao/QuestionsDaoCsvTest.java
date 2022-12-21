ac112eb659ba0ebddca130386d1932e7e0891d97
Dankoy <transingularity@gmail.com>
2022-12-21 18:14:57 +0300

    test: обновил тесты

--- hw-3/hw-3/src/test/java/ru/dankoy/core/dao/QuestionsDaoCsvTest.java
+++ hw-3/hw-3/src/test/java/ru/dankoy/core/dao/QuestionsDaoCsvTest.java
@@ -1,5 +1,6 @@
 package ru.dankoy.core.dao;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
@@ -15,6 +16,7 @@ import ru.dankoy.core.csvreader.CsvReader;
 import ru.dankoy.core.dao.csv.QuestionsDaoCsv;
 import ru.dankoy.core.domain.Answer;
 import ru.dankoy.core.domain.Question;
+import ru.dankoy.core.exceptions.QuestionsDaoException;
 
 @DisplayName("Test CsvQuestionDaoImpl ")
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +40,17 @@ class QuestionsDaoCsvTest {
 
   }
 
+  @DisplayName("should throw QuestionsDaoException because of incorrect parsed csv data")
+  @Test
+  void shouldThrowQuestionsDaoException() {
+
+    given(csvReader.read()).willReturn(incorrectCsvRead());
+
+    assertThatThrownBy(() -> csvQuestionsDao.getQuestions())
+        .isInstanceOf(QuestionsDaoException.class);
+
+  }
+
   private List<Question> correctList() {
 
     List<Answer> answers = List.of(
@@ -58,6 +71,24 @@ class QuestionsDaoCsvTest {
 
     String[] keys = new String[]{
         "1", "Young's modulus is the property of",
+        "Gas only", "false",
+        "Both Solid and Liquid", "false",
+        "Liquid only", "false",
+        "Solid only", "true"
+    };
+
+    List<String[]> result = new ArrayList<>();
+
+    result.add(keys);
+
+    return result;
+
+  }
+
+  private List<String[]> incorrectCsvRead() {
+
+    String[] keys = new String[]{
+        "1", "error", "Young's modulus is the property of",
         "Gas only", "false",
         "Both Solid and Liquid", "false",
         "Liquid only", "false",
