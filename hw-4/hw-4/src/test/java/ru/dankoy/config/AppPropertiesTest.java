664344f75740bfe70587d1f2d3d8ddf6704dfa5c
Dankoy <transingularity@gmail.com>
2022-12-22 15:02:12 +0300

    test: обновил тесты

--- hw-4/hw-4/src/test/java/ru/dankoy/config/AppPropertiesTest.java
+++ hw-4/hw-4/src/test/java/ru/dankoy/config/AppPropertiesTest.java
@@ -6,17 +6,11 @@ import java.util.Locale;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ru.dankoy.startup.ApplicationStartup;
 
 @SpringBootTest
 class AppPropertiesTest {
-
-  // игнорируется бин стартапа, что бы билд проходил
-  @MockBean
-  private ApplicationStartup applicationStartup;
 
   @Autowired
   private AppProperties appProperties;
