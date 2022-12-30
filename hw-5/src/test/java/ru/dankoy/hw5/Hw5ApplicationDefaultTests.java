6389d254d50f09c0caf537ad07a716ad9c944515
Dankoy <transingularity@gmail.com>
2022-12-30 15:03:11 +0300

    test: обновил тесты

--- hw-5/src/test/java/ru/dankoy/hw5/Hw5ApplicationDefaultTests.java
+++ hw-5/src/test/java/ru/dankoy/hw5/Hw5ApplicationDefaultTests.java
@@ -13,26 +13,26 @@ import ru.dankoy.hw5.core.commands.AuthorCommand;
 import ru.dankoy.hw5.core.commands.BookCommand;
 import ru.dankoy.hw5.core.commands.GenreCommand;
 import ru.dankoy.hw5.core.dao.author.AuthorDaoJdbc;
-import ru.dankoy.hw5.core.dao.book.mergemanytomanybycode.BookDaoJdbcMerge;
+import ru.dankoy.hw5.core.dao.book.mergemanytomanybyjoin.BookDaoJdbcJoin;
 import ru.dankoy.hw5.core.dao.genre.GenreDaoJdbc;
 import ru.dankoy.hw5.core.service.author.AuthorServiceJdbc;
 import ru.dankoy.hw5.core.service.book.BookServiceJdbc;
 import ru.dankoy.hw5.core.service.genre.GenreServiceJdbc;
 import ru.dankoy.hw5.core.service.objectmapper.ObjectMapperServiceImpl;
 
-@DisplayName("Test context ")
+@DisplayName("Test default context ")
 @SpringBootTest
-class Hw5ApplicationTests {
+class Hw5ApplicationDefaultTests {
 
   @Autowired
   ApplicationContext context;
 
 
-  @DisplayName("all necessary beans should be created")
+  @DisplayName("all default necessary beans should be created")
   @Test
   void contextLoads() {
 
-    var bookDaoJdbc = context.getBean(BookDaoJdbcMerge.class);
+    var bookDaoJdbc = context.getBean(BookDaoJdbcJoin.class);
     var authorDaoJdbc = context.getBean(AuthorDaoJdbc.class);
     var genreDaoJdbc = context.getBean(GenreDaoJdbc.class);
     var bookServiceJdbc = context.getBean(BookServiceJdbc.class);
