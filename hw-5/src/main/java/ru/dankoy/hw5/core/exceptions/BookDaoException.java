04a8cad35e410ac8b7ac848d207ca624c531090a
Dankoy <transingularity@gmail.com>
2022-12-30 16:20:56 +0300

    refactor: удалил лишние конструкторы

--- hw-5/src/main/java/ru/dankoy/hw5/core/exceptions/BookDaoException.java
+++ hw-5/src/main/java/ru/dankoy/hw5/core/exceptions/BookDaoException.java
@@ -2,10 +2,6 @@ package ru.dankoy.hw5.core.exceptions;
 
 public class BookDaoException extends Hw5RootException {
 
-  public BookDaoException(String message) {
-    super(message);
-  }
-
   public BookDaoException(Throwable e) {
     super(e);
   }
