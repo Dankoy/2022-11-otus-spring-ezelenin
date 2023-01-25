43b0b6281fe7c5abebccdfa99cc9bd823b3fa61a
Dankoy <transingularity@gmail.com>
2023-01-25 11:28:05 +0300

    init: moved from previous hw7

---
+++ hw-8/src/main/java/ru/dankoy/hw7/core/commands/CommentaryCommand.java
@@ -0,0 +1,73 @@
+package ru.dankoy.hw7.core.commands;
+
+
+import java.util.HashSet;
+import javax.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import ru.dankoy.hw7.core.domain.Book;
+import ru.dankoy.hw7.core.domain.Commentary;
+import ru.dankoy.hw7.core.service.commentary.CommentaryService;
+import ru.dankoy.hw7.core.service.objectmapper.ObjectMapperService;
+
+
+@RequiredArgsConstructor
+@ShellComponent
+public class CommentaryCommand {
+
+  private final CommentaryService commentaryService;
+
+  private final ObjectMapperService objectMapperService;
+
+
+  @ShellMethod(key = {"commentary-get-by-id", "cgbi"}, value = "Get commentary by id")
+  public String getById(@ShellOption long id) {
+    var optional = commentaryService.getById(id);
+
+    // не уверен, нужно ли в контроллере обрабатывать null или делегировать логику в сервис
+    var author = optional.orElseThrow(
+        () -> new EntityNotFoundException(
+            String.format("No commentary has been found with id - %d", id))
+    );
+
+    return objectMapperService.convertToString(author);
+  }
+
+
+  @ShellMethod(key = {"commentary-get-all-by-book",
+      "cgabb"}, value = "Get all commentaries for book")
+  public String getAllByBookId(@ShellOption long bookId) {
+    var commentaries = commentaryService.getAllByBookId(bookId);
+    return objectMapperService.convertToString(commentaries);
+  }
+
+
+  @ShellMethod(key = {"commentary-insert", "ci"}, value = "Insert new commentary")
+  public String insert(@ShellOption long bookId, @ShellOption String comment) {
+    var book = new Book(bookId, null, new HashSet<>(), new HashSet<>(), new HashSet<>());
+    var commentary = new Commentary(0L, comment, book);
+    var createdCommentary = commentaryService.insertOrUpdate(commentary);
+    return objectMapperService.convertToString(createdCommentary);
+  }
+
+  @ShellMethod(key = {"commentary-delete", "cd"}, value = "Delete commentary by id")
+  public String deleteById(@ShellOption long id) {
+    commentaryService.deleteById(id);
+    return String.format("Deleted commentary with id - %d", id);
+  }
+
+
+  @ShellMethod(key = {"commentary-update", "cu"}, value = "Update commentary")
+  public String update(@ShellOption long bookId, @ShellOption long commentaryId,
+      @ShellOption String commentaryText) {
+    var book = new Book(bookId, null, new HashSet<>(), new HashSet<>(), new HashSet<>());
+    var commentary = new Commentary(commentaryId, commentaryText, book);
+    commentaryService.insertOrUpdate(commentary);
+    return String.format("Updated commentary - %s",
+        objectMapperService.convertToString(commentary));
+  }
+
+
+}
---
+++ hw-8/src/test/java/ru/dankoy/hw7/Hw5ApplicationDefaultTests.java
@@ -0,0 +1,64 @@
+package ru.dankoy.hw7;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import ru.dankoy.hw7.core.commands.AuthorCommand;
+import ru.dankoy.hw7.core.commands.BookCommand;
+import ru.dankoy.hw7.core.commands.GenreCommand;
+import ru.dankoy.hw7.core.dto.mapper.BookMapper;
+import ru.dankoy.hw7.core.repository.author.AuthorRepository;
+import ru.dankoy.hw7.core.repository.book.BookRepository;
+import ru.dankoy.hw7.core.repository.genre.GenreRepository;
+import ru.dankoy.hw7.core.service.author.AuthorServiceJpa;
+import ru.dankoy.hw7.core.service.book.BookServiceJpa;
+import ru.dankoy.hw7.core.service.genre.GenreServiceJpa;
+import ru.dankoy.hw7.core.service.objectmapper.ObjectMapperServiceImpl;
+
+@DisplayName("Test default context ")
+@SpringBootTest
+class Hw5ApplicationDefaultTests {
+
+  @Autowired
+  ApplicationContext context;
+
+
+  @DisplayName("all default necessary beans should be created")
+  @Test
+  void contextLoads() {
+
+    var bookRepository = context.getBean(BookRepository.class);
+    var authorRepository = context.getBean(AuthorRepository.class);
+    var genreRepository = context.getBean(GenreRepository.class);
+    var bookServiceJpa = context.getBean(BookServiceJpa.class);
+    var authorServiceJpa = context.getBean(AuthorServiceJpa.class);
+    var genreServiceJpa = context.getBean(GenreServiceJpa.class);
+    var objectMapper = context.getBean(ObjectMapper.class);
+    var authorCommand = context.getBean(AuthorCommand.class);
+    var bookCommand = context.getBean(BookCommand.class);
+    var genreCommand = context.getBean(GenreCommand.class);
+    var objectMapperService = context.getBean(ObjectMapperServiceImpl.class);
+    var bookMapper = context.getBean(BookMapper.class);
+
+    assertNotNull(bookRepository);
+    assertNotNull(genreRepository);
+    assertNotNull(authorRepository);
+    assertNotNull(bookServiceJpa);
+    assertNotNull(authorServiceJpa);
+    assertNotNull(genreServiceJpa);
+    assertNotNull(objectMapper);
+    assertNotNull(authorCommand);
+    assertNotNull(bookCommand);
+    assertNotNull(genreCommand);
+    assertNotNull(objectMapperService);
+    assertNotNull(bookMapper);
+
+  }
+
+}
