f476d680255f479d18bf5691d5a076d5f622d17d
Dankoy <transingularity@gmail.com>
2023-01-25 16:16:23 +0300

    refactor: removed ref to book from commentary

--- hw-8/src/main/java/ru/dankoy/hw8/core/domain/Commentary.java
+++ hw-8/src/main/java/ru/dankoy/hw8/core/domain/Commentary.java
@@ -1,23 +1,19 @@
 package ru.dankoy.hw8.core.domain;
 
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = "book")
+@EqualsAndHashCode
 // фиксит рекурсивный вызов equals и hashcode комментария и книги
 @Getter
-@Document("commentary")
 public class Commentary {
 
   @Id
@@ -26,8 +22,5 @@ public class Commentary {
   @Field("text")
   private String text;
 
-  @DBRef
-  @JsonBackReference // решает проблему с рекурсивной сериализацией
-  private Book book;
 
 }
