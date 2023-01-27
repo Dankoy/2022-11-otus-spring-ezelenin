7ee1b0c32614eecab2e60ee1dd6f658a32953e8b
Dankoy <transingularity@gmail.com>
2023-01-27 15:35:15 +0300

    refactor: added tostring

--- hw-8/src/main/java/ru/dankoy/hw8/core/domain/Author.java
+++ hw-8/src/main/java/ru/dankoy/hw8/core/domain/Author.java
@@ -6,11 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
--- hw-8/src/main/java/ru/dankoy/hw8/core/domain/Genre.java
+++ hw-8/src/main/java/ru/dankoy/hw8/core/domain/Genre.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Getter
+@ToString
 public class Genre {
 
   @Field("name")
