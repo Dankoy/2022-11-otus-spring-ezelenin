f5fe06d3a9336c0ca4954e84c1f19bdb2a335fbc
Dankoy <transingularity@gmail.com>
2023-01-28 11:48:09 +0300

    refactor: добавил удаление автора из книг

--- hw-8/src/main/java/ru/dankoy/hw8/core/service/author/AuthorServiceMongo.java
+++ hw-8/src/main/java/ru/dankoy/hw8/core/service/author/AuthorServiceMongo.java
@@ -6,14 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.dankoy.hw8.core.domain.Author;
+import ru.dankoy.hw8.core.domain.Book;
 import ru.dankoy.hw8.core.exceptions.EntityNotFoundException;
 import ru.dankoy.hw8.core.repository.author.AuthorRepository;
+import ru.dankoy.hw8.core.service.book.BookService;
 
 @Service
 @RequiredArgsConstructor
 public class AuthorServiceMongo implements AuthorService {
 
   private final AuthorRepository authorRepository;
+
+  private final BookService bookService;
 
   @Override
   public List<Author> getAll() {
@@ -37,6 +41,13 @@ public class AuthorServiceMongo implements AuthorService {
     var optional = authorRepository.findById(id);
     var author = optional.orElseThrow(() -> new EntityNotFoundException(
         String.format("Entity %s has not been found with id - %s", Author.class.getName(), id)));
+
+    List<Book> books = bookService.findAllByAuthorId(author);
+
+    books.forEach(b -> b.getAuthors().remove(author));
+
+    bookService.updateMultiple(books);
+
     authorRepository.delete(author);
   }
 
