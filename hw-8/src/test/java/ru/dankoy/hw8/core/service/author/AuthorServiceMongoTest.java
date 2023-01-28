44823067905d1bca1b903bb70d0d4c6316d67ffd
Dankoy <transingularity@gmail.com>
2023-01-28 10:11:09 +0300

    test: updated author service tests

--- hw-8/src/test/java/ru/dankoy/hw8/core/service/author/AuthorServiceMongoTest.java
+++ hw-8/src/test/java/ru/dankoy/hw8/core/service/author/AuthorServiceMongoTest.java
@@ -13,9 +13,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.dankoy.hw8.core.domain.Author;
@@ -24,130 +27,117 @@ import ru.dankoy.hw8.core.repository.author.AuthorRepository;
 
 
 @Transactional(propagation = Propagation.NEVER)
-@DisplayName("Test AuthorServiceJpa ")
-//@DataJpaTest
+@DisplayName("Test AuthorServiceMongo ")
+@DataMongoTest
 @Import({AuthorServiceMongo.class})
 class AuthorServiceMongoTest {
 
+  @MockBean
+  private AuthorRepository authorRepository;
 
-//  @MockBean
-//  private AuthorRepository authorRepository;
-//
-//  @Autowired
-//  private AuthorServiceMongo authorServiceJdbc;
-//
-//
-//  @DisplayName("should return all authors")
-//  @Test
-//  void shouldGetAllAuthorsTest() {
-//
-//    given(authorRepository.findAll()).willReturn(makeCorrectAllAuthorsList());
-//
-//    var authors = authorServiceJdbc.getAll();
-//
-//    assertThat(authors).isEqualTo(makeCorrectAllAuthorsList());
-//
-//    Mockito.verify(authorRepository, times(1)).findAll();
-//  }
-//
-//
-//  @DisplayName("should return correct count")
-//  @Test
-//  void shouldReturnCorrectCountTest() {
-//
-//    given(authorRepository.count()).willReturn(3L);
-//
-//    var count = authorServiceJdbc.count();
-//
-//    assertThat(count).isEqualTo(makeCorrectAllAuthorsList().size());
-//    Mockito.verify(authorRepository, times(1)).count();
-//
-//  }
-//
-//  @DisplayName("should return correct author by id")
-//  @Test
-//  void shouldReturnCorrectAuthorById() {
-//
-//    var id = 1;
-//
-//    var authors = makeCorrectAllAuthorsList();
-//    var correctAuthor = getAuthorByIdFromList(authors, id);
-//
-//    given(authorRepository.getById(id)).willReturn(Optional.ofNullable(correctAuthor));
-//
-//    var author = authorServiceJdbc.getById(id);
-//
-//    assertThat(author).isPresent().get().isEqualTo(correctAuthor);
-//    Mockito.verify(authorRepository, times(1)).getById(id);
-//
-//  }
-//
-//  @DisplayName("should correctly insert author in db")
-//  @Test
-//  void shouldCorrectlyInsertAuthor() {
-//
-//    var authorName = "author4";
-//    var insertedId = 4L;
-//
-//    var authorToInsert = new Author(0L, authorName);
-//    var insertedAuthor = new Author(insertedId, authorName);
-//
-//    given(authorRepository.save(authorToInsert)).willReturn(insertedAuthor);
-//
-//    var actual = authorServiceJdbc.insertOrUpdate(authorToInsert);
-//
-//    assertThat(actual).isEqualTo(insertedAuthor);
-//    Mockito.verify(authorRepository, times(1)).save(authorToInsert);
-//
-//  }
-//
-//  @DisplayName("should correctly delete author by id")
-//  @Test
-//  void shouldCorrectlyDeleteAuthorById() {
-//
-//    var id = 1L;
-//
-//    var toDelete = new Author(id, "name");
-//    given(authorRepository.getById(id)).willReturn(Optional.of(toDelete));
-//
-//    authorServiceJdbc.deleteById(id);
-//
-//    Mockito.verify(authorRepository, times(1)).delete(toDelete);
-//
-//  }
-//
-//  @DisplayName("should correctly delete author by id")
-//  @Test
-//  void shouldThrowExceptionWhenDeleteNonExistingAuthorById() {
-//
-//    var id = 999L;
-//
-//    given(authorRepository.getById(id)).willReturn(Optional.empty());
-//
-//    assertThatThrownBy(() -> authorServiceJdbc.deleteById(id))
-//        .isInstanceOf(EntityNotFoundException.class);
-//
-//    Mockito.verify(authorRepository, times(0)).delete(any());
-//
-//  }
-//
-//
-//  private Author getAuthorByIdFromList(List<Author> authors, long id) {
-//
-//    var authorOptional = authors.stream().filter(author -> author.getId() == id)
-//        .findFirst();
-//
-//    return authorOptional.orElse(new Author(999999L, "nonexisting"));
-//
-//  }
-//
-//  private List<Author> makeCorrectAllAuthorsList() {
-//    return List.of(
-//        new Author(1L, "author1"),
-//        new Author(2L, "author2"),
-//        new Author(3L, "author3")
-//    );
-//  }
+  @Autowired
+  private AuthorServiceMongo authorServiceMongo;
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+
+  @DisplayName("should return all authors")
+  @Test
+  void shouldGetAllAuthorsTest() {
+
+    var correctAuthorList = makeCorrectAllAuthorsList();
+
+    given(authorRepository.findAll()).willReturn(correctAuthorList);
+
+    var authors = authorServiceMongo.getAll();
+
+    assertThat(authors).isEqualTo(correctAuthorList);
+
+    Mockito.verify(authorRepository, times(1)).findAll();
+  }
+
+
+  @DisplayName("should return correct count")
+  @Test
+  void shouldReturnCorrectCountTest() {
+
+    given(authorRepository.count()).willReturn(4L);
+
+    var count = authorServiceMongo.count();
+
+    assertThat(count).isEqualTo(makeCorrectAllAuthorsList().size());
+    Mockito.verify(authorRepository, times(1)).count();
+
+  }
+
+  @DisplayName("should return correct author by id")
+  @Test
+  void shouldReturnCorrectAuthorById() {
+
+    var expectedAuthor = mongoTemplate.find(new Query()
+            .addCriteria(Criteria.where("name").is("author1")), Author.class)
+        .get(0);
+
+    given(authorRepository.findById(expectedAuthor.getId()))
+        .willReturn(Optional.of(expectedAuthor));
+
+    var actualAuthor = authorServiceMongo.getById(expectedAuthor.getId());
+
+    assertThat(actualAuthor).isPresent().get().isEqualTo(expectedAuthor);
+    Mockito.verify(authorRepository, times(1)).findById(expectedAuthor.getId());
+
+  }
+
+  @DisplayName("should correctly insert author in db")
+  @Test
+  void shouldCorrectlyInsertAuthor() {
+
+    var authorToInsert = new Author(null, "new_author");
+    var insertedAuthor = new Author("whatever", "new_author");
+
+    given(authorRepository.save(authorToInsert)).willReturn(insertedAuthor);
+
+    var actual = authorServiceMongo.insertOrUpdate(authorToInsert);
+
+    assertThat(actual).isEqualTo(insertedAuthor);
+    Mockito.verify(authorRepository, times(1)).save(authorToInsert);
+
+  }
+
+  @DisplayName("should correctly delete author by id")
+  @Test
+  void shouldCorrectlyDeleteAuthorById() {
+
+    var id = "1L";
+
+    var toDelete = new Author(id, "name");
+    given(authorRepository.findById(id)).willReturn(Optional.of(toDelete));
+
+    authorServiceMongo.deleteById(id);
+
+    Mockito.verify(authorRepository, times(1)).delete(toDelete);
+
+  }
+
+  @DisplayName("should throw exception when deleting author by id")
+  @Test
+  void shouldThrowExceptionWhenDeleteNonExistingAuthorById() {
+
+    var id = "999";
+
+    given(authorRepository.findById(id)).willReturn(Optional.empty());
+
+    assertThatThrownBy(() -> authorServiceMongo.deleteById(id))
+        .isInstanceOf(EntityNotFoundException.class);
+
+    Mockito.verify(authorRepository, times(0)).delete(any());
+
+  }
+
+  private List<Author> makeCorrectAllAuthorsList() {
+    return mongoTemplate.findAll(Author.class);
+  }
 
 
 }
