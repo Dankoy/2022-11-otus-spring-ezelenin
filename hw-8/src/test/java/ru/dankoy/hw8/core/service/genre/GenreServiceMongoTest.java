780480a60392c13dfe2710df46f6a98f74580f45
Dankoy <transingularity@gmail.com>
2023-01-28 11:06:20 +0300

    test: updated genre service test

--- hw-8/src/test/java/ru/dankoy/hw8/core/service/genre/GenreServiceMongoTest.java
+++ hw-8/src/test/java/ru/dankoy/hw8/core/service/genre/GenreServiceMongoTest.java
@@ -1,148 +1,157 @@
 package ru.dankoy.hw8.core.service.genre;
 
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import ru.dankoy.hw8.core.domain.Author;
+import ru.dankoy.hw8.core.domain.Book;
 import ru.dankoy.hw8.core.domain.Genre;
-import ru.dankoy.hw8.core.exceptions.EntityNotFoundException;
+import ru.dankoy.hw8.core.service.book.BookService;
 
 
 @Transactional(propagation = Propagation.NEVER)
 @DisplayName("Test GenreServiceJpa ")
-//@JdbcTest
+@DataMongoTest
 @Import({GenreServiceMongo.class})
 class GenreServiceMongoTest {
 
-//  @MockBean
-//  private GenreRepository genreRepository;
-//
-//  @Autowired
-//  private GenreServiceMongo genreServiceJpa;
-//
-//
-//  @DisplayName("should return all genres")
-//  @Test
-//  void shouldGetAllGenresTest() {
-//
-//    given(genreRepository.findAll()).willReturn(makeCorrectAllGenresList());
-//
-//    var genres = genreServiceJpa.getAll();
-//
-//    assertThat(genres).isEqualTo(makeCorrectAllGenresList());
-//    Mockito.verify(genreRepository, times(1)).findAll();
-//  }
-//
-//
-//  @DisplayName("should return correct count")
-//  @Test
-//  void shouldReturnCorrectCountTest() {
-//
-//    given(genreRepository.count()).willReturn(3L);
-//    var count = genreServiceJpa.count();
-//
-//    assertThat(count).isEqualTo(makeCorrectAllGenresList().size());
-//    Mockito.verify(genreRepository, times(1)).count();
-//
-//  }
-//
-//  @DisplayName("should return correct genre by id")
-//  @Test
-//  void shouldReturnCorrectGenreById() {
-//
-//    var id = 1;
-//
-//    var genres = makeCorrectAllGenresList();
-//    var correctgenre = getGenreByIdFromList(genres, id);
-//
-//    given(genreRepository.getById(id)).willReturn(Optional.ofNullable(correctgenre));
-//
-//    var genre = genreServiceJpa.getById(id);
-//
-//    assertThat(genre).isPresent().get().isEqualTo(correctgenre);
-//    Mockito.verify(genreRepository, times(1)).getById(id);
-//
-//  }
-//
-//  @DisplayName("should correctly insert genre in db")
-//  @Test
-//  void shouldCorrectlyInsertGenre() {
-//
-//    var genreName = "genre4";
-//    var insertedId = 4L;
-//
-//    var genreToInsert = new Genre(0L, genreName);
-//    var insertedGenre = new Genre(insertedId, genreName);
-//
-//    given(genreRepository.save(genreToInsert)).willReturn(insertedGenre);
-//
-//    var actual = genreServiceJpa.insertOrUpdate(genreToInsert);
-//
-//    assertThat(actual).isEqualTo(insertedGenre);
-//    Mockito.verify(genreRepository, times(1)).save(genreToInsert);
-//
-//  }
-//
-//  @DisplayName("should correctly delete genre by id")
-//  @Test
-//  void shouldCorrectlyDeleteGenreById() {
-//
-//    var id = 1L;
-//
-//    var toDelete = new Genre(id, "name");
-//    given(genreRepository.getById(id)).willReturn(Optional.of(toDelete));
-//    genreServiceJpa.deleteById(id);
-//
-//    Mockito.verify(genreRepository, times(1)).delete(toDelete);
-//
-//  }
-//
-//  @DisplayName("should throw exception when delete genre by id")
-//  @Test
-//  void shouldThrowExceptionWhenDeleteNonExistingGenreById() {
-//
-//    var id = 999L;
-//
-//    given(genreRepository.getById(id)).willReturn(Optional.empty());
-//
-//    assertThatThrownBy(() -> genreServiceJpa.deleteById(id))
-//        .isInstanceOf(EntityNotFoundException.class);
-//
-//    Mockito.verify(genreRepository, times(0)).delete(any());
-//
-//  }
-//
-//
-//  private Genre getGenreByIdFromList(List<Genre> genres, long id) {
-//
-//    var genreOptional = genres.stream().filter(genre -> genre.getId() == id)
-//        .findFirst();
-//
-//    return genreOptional.orElse(new Genre(999999L, "nonexisting"));
-//
-//  }
-//
-//  private List<Genre> makeCorrectAllGenresList() {
-//    return List.of(
-//        new Genre(1L, "genre1"),
-//        new Genre(2L, "genre2"),
-//        new Genre(3L, "genre3")
-//    );
-//  }
+  @MockBean
+  private BookService bookService;
+
+  @Autowired
+  private GenreServiceMongo genreServiceMongo;
+
+
+  @DisplayName("should return all genres")
+  @Test
+  void shouldGetAllGenresTest() {
+
+    given(bookService.findAll()).willReturn(makeCorrectAllBooksList());
+
+    var genres = genreServiceMongo.getAllGenres();
+
+    assertThat(genres).isEqualTo(makeCorrectAllGenresList());
+    Mockito.verify(bookService, times(1)).findAll();
+  }
+
+
+  @DisplayName("should correctly update genres")
+  @Test
+  void shouldUpdateGenre() {
+
+    var oldGenre = new Genre("genre1");
+    var newGenre = new Genre("new");
+    var booksReturnFromService = makeCorrectAllBooksList();
+    var expectedBooks = makeCorrectAllBooksList();
+
+    given(bookService.findAllByGenreName(oldGenre)).willReturn(booksReturnFromService);
+
+    booksAfterUpdateGenres(expectedBooks, oldGenre, newGenre);
+
+    genreServiceMongo.update(oldGenre, newGenre);
+
+    Mockito.verify(bookService, times(1))
+        .updateMultiple(expectedBooks);
+    Mockito.verify(bookService, times(1))
+        .findAllByGenreName(oldGenre);
+    assertThat(expectedBooks).containsExactlyInAnyOrderElementsOf(booksReturnFromService);
+
+  }
+
+  @DisplayName("should correctly delete genres from books")
+  @Test
+  void shouldDeleteGenre() {
+
+    var toDelete = new Genre("genre1");
+    var booksReturnFromService = makeCorrectAllBooksList();
+    var expectedBooks = makeCorrectAllBooksList();
+
+    given(bookService.findAllByGenreName(toDelete)).willReturn(booksReturnFromService);
+
+    booksAfterDeleteGenres(expectedBooks, toDelete);
+
+    genreServiceMongo.delete(toDelete);
+
+    Mockito.verify(bookService, times(1)).updateMultiple(expectedBooks);
+    Mockito.verify(bookService, times(1)).findAllByGenreName(toDelete);
+    assertThat(expectedBooks).containsExactlyInAnyOrderElementsOf(booksReturnFromService);
+
+  }
+
+  private void booksAfterUpdateGenres(List<Book> books, Genre oldGenre, Genre newGenre) {
+
+    books.forEach(b -> {
+      b.getGenres().remove(oldGenre);
+      b.getGenres().add(newGenre);
+    });
+
+  }
+
+  private void booksAfterDeleteGenres(List<Book> books, Genre toDelete) {
+
+    books.forEach(b -> b.getGenres().remove(toDelete));
+
+  }
+
+
+  private Set<Genre> makeCorrectAllGenresList() {
+    return Set.of(
+        new Genre("genre1"),
+        new Genre("genre2"),
+        new Genre("genre3")
+    );
+  }
+
+
+  private List<Book> makeCorrectAllBooksList() {
+
+    Set<Genre> genreBook1 = new HashSet<>();
+    genreBook1.add(new Genre("genre1"));
+    genreBook1.add(new Genre("genre2"));
+
+    Set<Genre> genreBook2 = new HashSet<>();
+    genreBook2.add(new Genre("genre2"));
+    genreBook2.add(new Genre("genre3"));
+
+    Set<Genre> genreBook3 = new HashSet<>();
+    genreBook3.add(new Genre("genre1"));
+    genreBook3.add(new Genre("genre3"));
+
+    var book1 = new Book("1L", "book1",
+        Set.of(new Author("1L", "author1"), new Author("2L", "author2")),
+        genreBook1,
+        new HashSet<>());
+
+    var book2 = new Book("2L", "book2",
+        Set.of(new Author("2L", "author2"), new Author("3L", "author3")),
+        genreBook2,
+        new HashSet<>());
+
+    var book3 = new Book("3L", "book3",
+        Set.of(new Author("1L", "author1"), new Author("3L", "author3")),
+        genreBook3,
+        new HashSet<>());
+
+    return List.of(
+        book1,
+        book2,
+        book3
+    );
+  }
 
 
 }
