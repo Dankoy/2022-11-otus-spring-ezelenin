6a7592cfdb3937091e0b4697aa269fe494875ef5
Dankoy <transingularity@gmail.com>
2023-02-09 15:48:05 +0300

    refactor: updated dtomapper

--- hw-9/src/main/java/ru/dankoy/hw7/core/dto/mapper/BookMapper.java
+++ hw-9/src/main/java/ru/dankoy/hw7/core/dto/mapper/BookMapper.java
@@ -2,6 +2,7 @@ package ru.dankoy.hw7.core.dto.mapper;
 
 import ru.dankoy.hw7.core.domain.Book;
 import ru.dankoy.hw7.core.dto.BookDTO;
+import ru.dankoy.hw7.core.dto.BookFormDTO;
 
 public interface BookMapper {
 
@@ -10,4 +11,8 @@ public interface BookMapper {
   BookDTO toSimpleDTO(Book book);
 
   Book toBook(BookDTO dto);
+
+  BookFormDTO bookFormDTO(Book book);
+
+  Book toBook(BookFormDTO bookFormDTO);
 }
--- hw-9/src/main/java/ru/dankoy/hw7/core/dto/mapper/BookMapperImpl.java
+++ hw-9/src/main/java/ru/dankoy/hw7/core/dto/mapper/BookMapperImpl.java
@@ -1,9 +1,13 @@
 package ru.dankoy.hw7.core.dto.mapper;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
+import ru.dankoy.hw7.core.domain.Author;
 import ru.dankoy.hw7.core.domain.Book;
+import ru.dankoy.hw7.core.domain.Genre;
 import ru.dankoy.hw7.core.dto.BookDTO;
+import ru.dankoy.hw7.core.dto.BookFormDTO;
 
 
 @Component
@@ -45,6 +49,31 @@ public class BookMapperImpl implements BookMapper {
         dto.getAuthors(),
         dto.getGenres(),
         dto.getCommentaries()
+    );
+
+  }
+
+  @Override
+  public BookFormDTO bookFormDTO(Book book) {
+
+    return BookFormDTO.builder()
+        .id(book.getId())
+        .name(book.getName())
+        .genres(book.getGenres().stream().map(Genre::getId).collect(Collectors.toSet()))
+        .authors(book.getAuthors().stream().map(Author::getId).collect(Collectors.toSet()))
+        .build();
+
+  }
+
+  @Override
+  public Book toBook(BookFormDTO bookFormDTO) {
+
+    return new Book(
+        bookFormDTO.getId(),
+        bookFormDTO.getName(),
+        bookFormDTO.getAuthors().stream().map(Author::new).collect(Collectors.toSet()),
+        bookFormDTO.getGenres().stream().map(Genre::new).collect(Collectors.toSet()),
+        new HashSet<>()
     );
 
   }
