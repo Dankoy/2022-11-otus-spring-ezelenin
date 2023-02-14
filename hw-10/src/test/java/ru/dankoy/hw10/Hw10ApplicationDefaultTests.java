8c75c8e9a027f5ecf8b2cb282870101c304b41a4
Dankoy <transingularity@gmail.com>
2023-02-14 13:50:34 +0300

    refactor: removed shell commands

--- hw-10/src/test/java/ru/dankoy/hw10/Hw10ApplicationDefaultTests.java
+++ hw-10/src/test/java/ru/dankoy/hw10/Hw10ApplicationDefaultTests.java
@@ -9,9 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import ru.dankoy.hw10.core.commands.AuthorCommand;
-import ru.dankoy.hw10.core.commands.BookCommand;
-import ru.dankoy.hw10.core.commands.GenreCommand;
 import ru.dankoy.hw10.core.dto.mapper.BookMapper;
 import ru.dankoy.hw10.core.repository.author.AuthorRepository;
 import ru.dankoy.hw10.core.repository.book.BookRepository;
@@ -38,9 +35,6 @@ class Hw10ApplicationDefaultTests {
     var authorServiceJpa = context.getBean(AuthorServiceMongo.class);
     var genreServiceJpa = context.getBean(GenreServiceMongo.class);
     var objectMapper = context.getBean(ObjectMapper.class);
-    var authorCommand = context.getBean(AuthorCommand.class);
-    var bookCommand = context.getBean(BookCommand.class);
-    var genreCommand = context.getBean(GenreCommand.class);
     var objectMapperService = context.getBean(ObjectMapperServiceImpl.class);
     var bookMapper = context.getBean(BookMapper.class);
 
@@ -50,9 +44,7 @@ class Hw10ApplicationDefaultTests {
     assertNotNull(authorServiceJpa);
     assertNotNull(genreServiceJpa);
     assertNotNull(objectMapper);
-    assertNotNull(authorCommand);
-    assertNotNull(bookCommand);
-    assertNotNull(genreCommand);
+
     assertNotNull(objectMapperService);
     assertNotNull(bookMapper);
 
