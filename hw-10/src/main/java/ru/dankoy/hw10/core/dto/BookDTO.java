891e8e2846ace55320987f095dd465ac4f7ef2f3
Dankoy <transingularity@gmail.com>
2023-02-17 14:19:47 +0300

    refactor: updated bookdto

--- hw-10/src/main/java/ru/dankoy/hw10/core/dto/BookDTO.java
+++ hw-10/src/main/java/ru/dankoy/hw10/core/dto/BookDTO.java
@@ -3,21 +3,27 @@ package ru.dankoy.hw10.core.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import ru.dankoy.hw10.core.domain.Author;
 import ru.dankoy.hw10.core.domain.Commentary;
 import ru.dankoy.hw10.core.domain.Genre;
 
+@ToString
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(Include.NON_EMPTY)
 public class BookDTO {
 
-  private final String id;
+  private String id;
 
-  private final String name;
+  private String name;
 
   private Set<Author> authors;
 
