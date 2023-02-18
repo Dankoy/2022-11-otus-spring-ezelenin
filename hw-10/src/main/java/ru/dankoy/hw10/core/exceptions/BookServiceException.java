c992e0c73ad443d70de9537df4056cdd9e9af66c
Dankoy <transingularity@gmail.com>
2023-02-18 04:10:29 +0300

    refactor: deleted unnecessary exception

--- hw-10/src/main/java/ru/dankoy/hw10/core/exceptions/BookServiceException.java
+++
@@ -1,9 +0,0 @@
-package ru.dankoy.hw10.core.exceptions;
-
-public class BookServiceException extends Hw5RootException {
-
-  public BookServiceException(String message) {
-    super(message);
-  }
-
-}
