8df7296a8764799408639154ba6cc3d375195390
Dankoy <transingularity@gmail.com>
2023-03-09 14:03:39 +0300

    refactor: added session management

--- hw-12/src/main/java/ru/dankoy/hw12/config/security/SecurityConfiguration.java
+++ hw-12/src/main/java/ru/dankoy/hw12/config/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package ru.dankoy.hw12.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +16,8 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+        .and()
         .authorizeHttpRequests(authorize ->
             authorize.anyRequest().authenticated()
         )
