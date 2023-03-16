f54bc3d916b6d8618acfca72de7cc138f1fe3619
Dankoy <transingularity@gmail.com>
2023-03-16 15:09:21 +0300

    refactor: added graph to retrieve user roles

--- hw-13/src/main/java/ru/dankoy/hw13/core/repository/user/UserRepository.java
+++ hw-13/src/main/java/ru/dankoy/hw13/core/repository/user/UserRepository.java
@@ -1,11 +1,13 @@
 package ru.dankoy.hw13.core.repository.user;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.dankoy.hw13.core.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  @EntityGraph(value = "user-to-roles-graph")
   Optional<User> findByUsername(String username);
 
 }
