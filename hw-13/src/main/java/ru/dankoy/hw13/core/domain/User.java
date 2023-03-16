5b4635b96f9a53fcc3ca982235ea175f742f2faa
Dankoy <transingularity@gmail.com>
2023-03-16 15:09:06 +0300

    refactor: added roles for user

--- hw-13/src/main/java/ru/dankoy/hw13/core/domain/User.java
+++ hw-13/src/main/java/ru/dankoy/hw13/core/domain/User.java
@@ -1,15 +1,25 @@
 package ru.dankoy.hw13.core.domain;
 
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+@NamedEntityGraph(name = "user-to-roles-graph",
+    attributeNodes = {@NamedAttributeNode("roles")})
 @Entity
 @Table(name = "users")
 @Getter
@@ -39,5 +49,11 @@ public class User {
 
   @Column(name = "credentials_non_expired")
   private boolean credentialsNonExpired;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "users_roles",
+      joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+  private Set<UserRole> roles; // entity graph
 
 }
