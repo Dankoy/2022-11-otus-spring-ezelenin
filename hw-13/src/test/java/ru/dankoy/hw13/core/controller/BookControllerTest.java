6bb8b7a3651b89a3988b6fd42cf2e4047e8894dd
Dankoy <transingularity@gmail.com>
2023-03-18 08:40:56 +0300

    test: updated tests

--- hw-13/src/test/java/ru/dankoy/hw13/core/controller/BookControllerTest.java
+++ hw-13/src/test/java/ru/dankoy/hw13/core/controller/BookControllerTest.java
@@ -19,33 +19,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.dankoy.hw13.config.security.SecurityConfiguration;
 import ru.dankoy.hw13.core.domain.Author;
 import ru.dankoy.hw13.core.domain.Book;
 import ru.dankoy.hw13.core.domain.Commentary;
 import ru.dankoy.hw13.core.domain.Genre;
 import ru.dankoy.hw13.core.dto.book.BookDTO;
-import ru.dankoy.hw13.core.dto.book.mapper.BookMapper;
 import ru.dankoy.hw13.core.dto.book.mapper.BookMapperImpl;
 import ru.dankoy.hw13.core.service.author.AuthorService;
 import ru.dankoy.hw13.core.service.book.BookDtoService;
-import ru.dankoy.hw13.core.service.book.BookDtoServiceImpl;
 import ru.dankoy.hw13.core.service.genre.GenreService;
 
 
 @DisplayName("BookController test ")
-@WebMvcTest({BookController.class, BookDtoServiceImpl.class, BookMapperImpl.class})
-@AutoConfigureMockMvc(addFilters = false) // позволяет не получать ошибку 403 forbidden в post методах с параметрами
+@WebMvcTest(controllers = {BookController.class})
+@Import({SecurityConfiguration.class,
+    BookMapperImpl.class}) // надо обязательно имортировать конфиг, иначе фильтров не будет
 class BookControllerTest {
 
   @Autowired
   private MockMvc mvc;
   @Autowired
-  private BookMapper bookMapper;
+  private BookMapperImpl bookMapper;
 
   @MockBean
   private BookDtoService bookDtoService;
@@ -59,7 +59,8 @@ class BookControllerTest {
 
   @DisplayName("should correctly print all books")
   @WithMockUser(
-      username = "user"
+      username = "user",
+      roles = {"ADMIN", "OPERATOR", "READER"}
   )
   @Test
   void shouldReturnCorrectBooksList() throws Exception {
@@ -78,9 +79,30 @@ class BookControllerTest {
 
   }
 
+  @DisplayName("should throw 403 when get all books")
+  @WithMockUser(
+      username = "user",
+      roles = {"WEIRD"}
+  )
+  @Test
+  void shouldThrow403WhenGetBooksList() throws Exception {
+    List<Book> books = makeCorrectAllBooksList();
+    List<BookDTO> bookDTOS = books.stream()
+        .map(bookMapper::toDTOWithoutCommentaries)
+        .collect(Collectors.toList());
+
+    given(bookDtoService.getAllWithAuthorsAndGenres()).willReturn(bookDTOS);
+
+    mvc.perform(get("/books"))
+        .andExpect(status().isForbidden());
+
+    Mockito.verify(bookDtoService, times(0)).getAllWithAuthorsAndGenres();
+
+  }
+
   @DisplayName("should correctly return book view")
   @WithMockUser(
-      username = "user"
+      roles = {"READER", "ADMIN", "OPERATOR"}
   )
   @Test
   void shouldGetBookById() throws Exception {
@@ -106,7 +128,8 @@ class BookControllerTest {
 
   @DisplayName("should correctly return create book view")
   @WithMockUser(
-      username = "user"
+      username = "none",
+      roles = {"ADMIN", "OPERATOR"}
   )
   @Test
   void shouldCreateBook() throws Exception {
@@ -130,10 +153,35 @@ class BookControllerTest {
 
   }
 
+  @DisplayName("should throw 403 create book view")
+  @WithMockUser(
+      roles = {"READER"}
+  )
+  @Test
+  void shouldThrow403ForReaderWhenCreateBook() throws Exception {
+
+    var bookId = 1L;
+
+    var books = makeCorrectAllBooksList();
+    var book = books.stream()
+        .filter(b -> b.getId() == bookId)
+        .findFirst().get();
+    var dto = Optional.of(bookMapper.toDTOWithCommentaries(book));
+
+    given(bookDtoService.getById(bookId)).willReturn(dto);
+
+    mvc.perform(get("/book/create"))
+        .andExpect(status().isForbidden());
+
+    Mockito.verify(authorService, times(0)).getAll();
+    Mockito.verify(genreService, times(0)).getAll();
+
+  }
+
   @DisplayName("should correctly redirect to book list after book creation")
   @WithMockUser(
       username = "user",
-      authorities = {"ROLE_ADMIN"}
+      roles = {"ADMIN", "OPERATOR"}
   )
   @Test
   void shouldReturnToBookListAfterCreation() throws Exception {
@@ -163,10 +211,42 @@ class BookControllerTest {
 
   }
 
+  @DisplayName("should throw 403 when redirect to book list after book creation")
+  @WithMockUser(
+      username = "user",
+      roles = {"READER"}
+  )
+  @Test
+  void shouldThrow403ForReaderWhenReturnToBookListAfterCreation() throws Exception {
+
+    var bookId = 1L;
+
+    var books = makeCorrectAllBooksList();
+    var book = books.stream()
+        .filter(b -> b.getId() == bookId)
+        .findFirst().get();
+    var dto = Optional.of(bookMapper.toDTOWithCommentaries(book));
+
+    mvc.perform(post("/book/create")
+            .param("id", String.valueOf(book.getId()))
+            .param("name", book.getName())
+            .param("_authors", "1")
+            .param("authors.id", "1")
+            .param("authors.id", "2")
+            .param("_genres", "1")
+            .param("genres.id", "1")
+            .param("genres.id", "2")
+        )
+        .andExpect(status().isForbidden());
+
+    Mockito.verify(bookDtoService, times(0)).insertOrUpdate(any());
+
+  }
+
 
   @DisplayName("should correctly open update book view")
   @WithMockUser(
-      username = "user"
+      roles = {"ADMIN", "OPERATOR"}
   )
   @Test
   void shouldCorrectlyOpenUpdateBookView() throws Exception {
@@ -194,7 +274,7 @@ class BookControllerTest {
   @DisplayName("should correctly update book and redirect to book list view")
   @WithMockUser(
       username = "user",
-      authorities = {"ROLE_ADMIN"}
+      authorities = {"ROLE_ADMIN", "ROLE_OPERATOR"}
   )
   @Test
   void shouldCorrectlyUpdateBookAndRedirectToBookListView() throws Exception {
@@ -227,9 +307,44 @@ class BookControllerTest {
 
   }
 
+  @DisplayName("should throw 403 when update book and redirect to book list view")
+  @WithMockUser(
+      username = "user",
+      authorities = {"READER"}
+  )
+  @Test
+  void shouldThrow403ForReaderWhenUpdateBookAndRedirectToBookListView() throws Exception {
+
+    var bookId = 1L;
+
+    var books = makeCorrectAllBooksList();
+    var book = books.stream()
+        .filter(b -> b.getId() == bookId)
+        .findFirst().get();
+    var dto = Optional.of(bookMapper.toDTOWithCommentaries(book));
+
+    given(bookDtoService.update(any())).willReturn(dto.get());
+
+    mvc.perform(post("/book/edit")
+            .param("id", String.valueOf(book.getId()))
+            .param("name", book.getName())
+            .param("_authors", "1")
+            .param("authors.id", "1")
+            .param("authors.id", "2")
+            .param("_genres", "1")
+            .param("genres.id", "1")
+            .param("genres.id", "2")
+        )
+        .andExpect(status().isForbidden());
+
+    Mockito.verify(bookDtoService, times(0)).update(any());
+
+  }
+
   @DisplayName("should correctly delete book and redirect to book list view")
   @WithMockUser(
-      username = "user"
+      username = "user",
+      roles = {"ADMIN"}
   )
   @Test
   void shouldCorrectlyDeleteBookAndRedirectToBookListView() throws Exception {
@@ -249,6 +364,32 @@ class BookControllerTest {
         .andExpect(view().name("redirect:/books"));
 
     Mockito.verify(bookDtoService, times(1)).deleteById(bookId);
+
+  }
+
+  @DisplayName("should throw 403 when delete book and redirect to book list view")
+  @WithMockUser(
+      username = "user",
+      roles = {"OPERATOR", "READER"}
+  )
+  @Test
+  void shouldThrow403ForOperatorAndReaderWhenDeleteBookAndRedirectToBookListView()
+      throws Exception {
+
+    var bookId = 1L;
+
+    var books = makeCorrectAllBooksList();
+    var book = books.stream()
+        .filter(b -> b.getId() == bookId)
+        .findFirst().get();
+    var dto = Optional.of(bookMapper.toDTOWithCommentaries(book));
+
+    given(bookDtoService.getById(bookId)).willReturn(dto);
+
+    mvc.perform(get("/book/delete?id=" + bookId))
+        .andExpect(status().isForbidden());
+
+    Mockito.verify(bookDtoService, times(0)).deleteById(bookId);
 
   }
 
