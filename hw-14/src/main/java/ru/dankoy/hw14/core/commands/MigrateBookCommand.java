c40f6f6bd2da6b1fee555767be829d226555767f
Dankoy <transingularity@gmail.com>
2023-03-24 16:19:30 +0300

    refactor: added get commentary by book id

--- hw-14/src/main/java/ru/dankoy/hw14/core/commands/MigrateBookCommand.java
+++ hw-14/src/main/java/ru/dankoy/hw14/core/commands/MigrateBookCommand.java
@@ -1,8 +1,10 @@
 package ru.dankoy.hw14.core.commands;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -11,9 +13,11 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 import ru.dankoy.hw14.core.repository.mongodb.BookMongoRepository;
 import ru.dankoy.hw14.core.repository.mongodb.CommentaryMongoRepository;
 
+@Slf4j
 @RequiredArgsConstructor
 @ShellComponent
 public class MigrateBookCommand {
@@ -32,12 +36,12 @@ public class MigrateBookCommand {
   public void startMigrationJobWithJobLauncher() throws Exception {
     JobExecution execution = jobLauncher.run(importUserJob, new JobParametersBuilder()
         .toJobParameters());
-    System.out.println(execution);
+    log.info("Execution - {}", execution);
   }
 
 
   @ShellMethod(value = "get-books-from-mongo", key = "gb")
-  public String getBooksFromMongo() throws Exception {
+  public String getBooksFromMongo() throws JsonProcessingException {
 
     var books = bookMongoRepository.findAll();
 
@@ -46,9 +50,9 @@ public class MigrateBookCommand {
 
 
   @ShellMethod(value = "getBooksFromMongo", key = "gc")
-  public String getCommentariesFromMongo() throws Exception {
+  public String getCommentariesFromMongo(@ShellOption String id) throws JsonProcessingException {
 
-    var books = commentaryMongoRepository.findByBookId("1");
+    var books = commentaryMongoRepository.findByBookId(id);
 
     return objectMapper.writeValueAsString(books);
   }
