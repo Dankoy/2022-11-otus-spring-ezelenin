f724cb72d483a666060723b68e913d3da78cc73e
Dankoy <transingularity@gmail.com>
2023-04-04 17:23:34 +0300

    feat: added hal for repos

--- hw-16/src/main/java/ru/dankoy/hw16/core/repository/author/AuthorRepository.java
+++ hw-16/src/main/java/ru/dankoy/hw16/core/repository/author/AuthorRepository.java
@@ -1,8 +1,11 @@
 package ru.dankoy.hw16.core.repository.author;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.dankoy.hw16.core.domain.Author;
 
+
+@RepositoryRestResource(path = "author")
 public interface AuthorRepository extends MongoRepository<Author, String> {
 
 
