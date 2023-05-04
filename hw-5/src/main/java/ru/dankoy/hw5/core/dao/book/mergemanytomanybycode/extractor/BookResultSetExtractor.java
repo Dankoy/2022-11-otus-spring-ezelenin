e612a264af265e27484f3139ce9aae2b714381b5
Evgeny <transingularity@gmail.com>
2023-05-04 11:05:42 +0300

    Hw 5 jdbc (#5)
    * chore: initial commit for homework 5
    
    * feat: добавил команды для жанра
    
    * refactor: переименовал классы
    
    * test: добавил настройки для тестов
    
    * refactor: добавил реализацию dao и сервиса для книг
    
    * refactor: исправил artifactId
    
    * test: стили
    
    * fix: пофиксил запрос
    
    * test: добавил тесты автора и жанров
    
    * refactor: добавил tostring
    
    * test: добавил тесты bok
    
    * test: добавил тесты сервисов
    
    * test: добавил тесты сервисов
    
    * chore: заменил бд с mem на файл
    
    * refactor: добавил связь many to many
    
    * refactor: добавил поверку на существование книги перед update
    
    * refactor: добавил поверку на существование сущности перед update
    
    * test: обновил тесты
    
    * refactor: убрал транзакции
    
    * test: обновил тесты
    
    * test: обновил тесты
    
    * test: обновил фикстуры для тестов
    
    * refactor: вынес старый класс дао в отдельный пакет
    
    * refactor: разделил логику с merge и join в bookdao
    
    * test: разделил логику с merge и join в bookdao
    
    * fixed: dao with join
    
    * test: тесты
    
    * refactor: вынес общий код в один пакет
    
    * test: обновил тесты
    
    * refactor: убрал тексты сообщений book dao
    
    * test: обновил тесты
    
    * refactor: удалил лишние конструкторы
    
    * test: обновил тесты
    
    * refactor: удалил тексты exceptions из дао
    
    * test: обновил тесты

---
+++ hw-5/src/main/java/ru/dankoy/hw5/core/dao/book/mergemanytomanybycode/extractor/BookResultSetExtractor.java
@@ -0,0 +1,27 @@
+package ru.dankoy.hw5.core.dao.book.mergemanytomanybycode.extractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import ru.dankoy.hw5.core.domain.Book;
+
+public class BookResultSetExtractor implements ResultSetExtractor<Map<Long, Book>> {
+
+  @Override
+  public Map<Long, Book> extractData(ResultSet rs) throws SQLException, DataAccessException {
+    Map<Long, Book> books = new HashMap<>();
+    while (rs.next()) {
+      long id = rs.getLong("id");
+      Book book = books.get(id);
+      if (book == null) {
+        book = new Book(id, rs.getString("name"), new ArrayList<>(), new ArrayList<>());
+        books.put(book.getId(), book);
+      }
+    }
+    return books;
+  }
+}
