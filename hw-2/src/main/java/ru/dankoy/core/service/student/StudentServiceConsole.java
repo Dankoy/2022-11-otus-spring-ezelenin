0a0f793846e9c5eb36e258796b44adab2eed051d
Evgeny <transingularity@gmail.com>
2023-05-04 11:05:04 +0300

    hw-2 (#2)
    * chore: moved files from first hw
    
    * refactor: use annotation based configuration for spring
    
    * refactor: added logic to perform testing
    
    * refactor: moven java config in separate package
    
    * test: moved questions test in separate package
    
    * refactor: added equals and hashcode
    
    * style: style
    
    * test: added new unit tests
    
    * test: added display name
    
    * refactor: changed property source
    
    * refactor: убрал принт результата тестирования в другой класс сервис
    
    * test: updated tests
    
    * test: updated tests
    
    * test: updated tests for usage of @Mock and @InjectMocks
    
    * refactor: вынес исключения в одно место
    
    * refactor: renamed class
    
    * refactor: removed field DI
    
    * refactor: added custom exception

---
+++ hw-2/src/main/java/ru/dankoy/core/service/student/StudentServiceConsole.java
@@ -0,0 +1,47 @@
+package ru.dankoy.core.service.student;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.dankoy.core.domain.Student;
+import ru.dankoy.core.service.io.IOService;
+
+/**
+ * @author turtality
+ * <p>
+ * Student service that gets student data from console
+ */
+@Service
+public class StudentServiceConsole implements StudentService {
+
+  private final IOService ioService;
+  @Value("${askStudentName}")
+  private String askStudentName;
+  @Value("${firstName}")
+  private String askFirstName;
+  @Value("${lastName}")
+  private String askLastName;
+
+  public StudentServiceConsole(IOService ioService) {
+    this.ioService = ioService;
+  }
+
+
+  /**
+   * Asks in console student data
+   *
+   * @return student {@link Student}
+   */
+  @Override
+  public Student getStudent() {
+
+    ioService.print(askStudentName);
+    ioService.print(askFirstName);
+    String firstName = ioService.readLn();
+
+    ioService.print(askLastName);
+    String lastName = ioService.readLn();
+
+    return new Student(firstName, lastName);
+
+  }
+}
---
+++ hw-2/src/main/java/ru/dankoy/core/service/test/TestingPerformer.java
@@ -0,0 +1,8 @@
+package ru.dankoy.core.service.test;
+
+import ru.dankoy.core.domain.TestResult;
+
+public interface TestingPerformer {
+
+  TestResult performTest();
+}
