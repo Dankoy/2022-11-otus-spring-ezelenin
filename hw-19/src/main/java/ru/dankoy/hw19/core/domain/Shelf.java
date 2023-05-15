a048b2f066c231b6143e95b68b123c7db2c182a3
Dankoy <transingularity@gmail.com>
2023-05-15 11:36:55 +0300

    refactor: added audit and doc reference

--- hw-19/src/main/java/ru/dankoy/hw19/core/domain/Shelf.java
+++ hw-19/src/main/java/ru/dankoy/hw19/core/domain/Shelf.java
@@ -4,11 +4,15 @@ package ru.dankoy.hw19.core.domain;
 import java.time.LocalDateTime;
 import java.util.Set;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -19,6 +23,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Getter
+@Builder
 @Document("shelves")
 public class Shelf {
 
@@ -28,6 +33,8 @@ public class Shelf {
   @Field("name")
   private String name;
 
+  @Setter
+  @DocumentReference(lookup = "{ '_id' : ?#{#target} }")
   @Field("user")
   private User user; // id
 
@@ -35,9 +42,12 @@ public class Shelf {
   @Field("editions")
   private Set<Edition> editions;
 
+  @CreatedDate
+  @Setter
   @Field("dt_created")
   private LocalDateTime dateCreated;
 
+  @LastModifiedDate
   @Field("dt_modified")
   private LocalDateTime dateModified;
 
