f9c2bceb329be7ab47bf2af6367117df0a57cf5c
Dankoy <transingularity@gmail.com>
2023-05-15 08:37:37 +0300

    refactor: added aspect

---
+++ hw-19/src/main/java/ru/dankoy/hw19/core/aspects/CreatedMetadataAspect.java
@@ -0,0 +1,48 @@
+package ru.dankoy.hw19.core.aspects;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import ru.dankoy.hw19.core.domain.Commentary;
+import ru.dankoy.hw19.core.domain.Work;
+import ru.dankoy.hw19.core.service.commentary.CommentaryService;
+import ru.dankoy.hw19.core.service.work.WorkService;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class CreatedMetadataAspect {
+
+  private final WorkService workService;
+  private final CommentaryService commentaryService;
+
+  @Before("@annotation(ru.dankoy.hw19.core.aspects.AddCreatedMetadata) && args(work)")
+  public void addCreatedMetadata(Work work) {
+
+    log.info("Aspect add meta to work");
+
+    var found = workService.getById(work.getId());
+
+    found.ifPresent(w -> {
+      work.setCreatedByUser(w.getCreatedByUser());
+      work.setDateCreated(w.getDateCreated());
+    });
+
+  }
+
+  @Before("@annotation(ru.dankoy.hw19.core.aspects.AddCreatedMetadata) && args(commentary)")
+  public void addCreatedMetadata(Commentary commentary) {
+
+    log.info("Aspect add meta to commentary");
+
+    var found = commentaryService.getById(commentary.getId());
+
+    found.ifPresent(w -> commentary.setDateCreated(w.getDateCreated()));
+
+  }
+
+}
