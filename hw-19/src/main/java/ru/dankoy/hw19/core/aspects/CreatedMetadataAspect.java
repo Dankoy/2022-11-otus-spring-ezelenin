f54ae6667a29dfd0d23ceb77a81a3eda6fd5151e
Dankoy <transingularity@gmail.com>
2023-05-15 09:40:17 +0300

    refactor: added pointcut for publisher

--- hw-19/src/main/java/ru/dankoy/hw19/core/aspects/CreatedMetadataAspect.java
+++ hw-19/src/main/java/ru/dankoy/hw19/core/aspects/CreatedMetadataAspect.java
@@ -7,8 +7,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import ru.dankoy.hw19.core.domain.Commentary;
+import ru.dankoy.hw19.core.domain.Publisher;
 import ru.dankoy.hw19.core.domain.Work;
 import ru.dankoy.hw19.core.service.commentary.CommentaryService;
+import ru.dankoy.hw19.core.service.publisher.PublisherService;
 import ru.dankoy.hw19.core.service.work.WorkService;
 
 @Slf4j
@@ -19,6 +21,7 @@ public class CreatedMetadataAspect {
 
   private final WorkService workService;
   private final CommentaryService commentaryService;
+  private final PublisherService publisherService;
 
   @Before("@annotation(ru.dankoy.hw19.core.aspects.AddCreatedMetadata) && args(work)")
   public void addCreatedMetadata(Work work) {
@@ -30,6 +33,20 @@ public class CreatedMetadataAspect {
     found.ifPresent(w -> {
       work.setCreatedByUser(w.getCreatedByUser());
       work.setDateCreated(w.getDateCreated());
+    });
+
+  }
+
+  @Before("@annotation(ru.dankoy.hw19.core.aspects.AddCreatedMetadata) && args(publisher)")
+  public void addCreatedMetadata(Publisher publisher) {
+
+    log.info("Aspect add meta to publisher");
+
+    var found = publisherService.findById(publisher.getId());
+
+    found.ifPresent(w -> {
+      publisher.setCreatedByUser(w.getCreatedByUser());
+      publisher.setDateCreated(w.getDateCreated());
     });
 
   }
