0d8499e1c6e9db54c88c4bb35804b90dc59eda07
Dankoy <transingularity@gmail.com>
2023-05-17 08:57:18 +0300

    refactor: added new method for repo

--- hw-19/src/main/java/ru/dankoy/hw19/core/repository/edition/EditionRepository.java
+++ hw-19/src/main/java/ru/dankoy/hw19/core/repository/edition/EditionRepository.java
@@ -1,10 +1,13 @@
 package ru.dankoy.hw19.core.repository.edition;
 
+import java.util.Set;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.dankoy.hw19.core.domain.Edition;
 
 @RepositoryRestResource(path = "edition")
 public interface EditionRepository extends MongoRepository<Edition, String> {
+
+  Set<Edition> findAllByWorkId(String workId);
 
 }
