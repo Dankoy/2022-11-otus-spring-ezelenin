a891ea232074974b5314160cd160ea14a6afe70a
Dankoy <transingularity@gmail.com>
2023-05-22 18:08:15 +0300

    refactor: updated audition config

--- hw-19/src/main/java/ru/dankoy/hw19/config/mongodbaudit/MongodbAuditionConfig.java
+++ hw-19/src/main/java/ru/dankoy/hw19/config/mongodbaudit/MongodbAuditionConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import ru.dankoy.hw19.core.domain.User;
 
 @EnableMongoAuditing
 @Configuration
 public class MongodbAuditionConfig {
 
   @Bean
-  public AuditorAware<String> myAuditorProvider() {
+  public AuditorAware<User> myAuditorProvider() {
     return new AuditorAwareImpl();
   }
 
